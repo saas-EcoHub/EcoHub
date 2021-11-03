801702b941d935531146b4e961a80b4040c0ce0e
Yihan Yin <yy3114@columbia.edu>
2021-11-03 06:21:17 -0400

    feature: eco hub 0.2

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,40 +1,64 @@
 class PromotionsController < ApplicationController
-    def index
-        @promotion = Promotion.all
+  def index
+    @all_categories = Promotion.all_categories
+    all_categories_map = Hash[@all_categories.map { |o| [o, '1'] }]
+    session[:categories] = all_categories_map unless session.key?(:categories)
+    session[:sort_by] = '' unless session.key?(:sort_by)
+    session[:categories] = if params.key?(:categories)
+                             params[:categories]
+                           else
+                             'Refresh'.eql?(params[:commit]) ? all_categories_map : session[:categories]
+                           end
+    session[:sort_by] = params.key?(:sort_by) ? params[:sort_by] : session[:sort_by]
+    unless params.key?(:categories) && params.key?(:sort_by)
+      redirect_to promotions_path(categories: session[:categories], sort_by: session[:sort_by]) and return
     end
-    
-    def new
-        @promotion = Promotion.new
-    end
-    
-    def create
-        @promotion = Promotion.create!(promotion_params)
-        redirect_to promotions_path
-    end
-    
-    def show
-        id = params[:id]
-        @promotion = Promotion.find(id)
-    end
-    
-    def destroy
-        @promotion = Promotion.find(params[:id])
-        @promotion.destroy
-        redirect_to promotions_path
-    end
-    
-    def edit
-        @promotion = Promotion.find(params[:id])
-    end
-    
-    def update
-        @promotion = Promotion.find params[:id]
-        @promotion.update_attributes!(promotion_params)
-        redirect_to promotion_path(@promotion)
-    end
-        
-    private
-    def promotion_params
-        params.require(:promotion).permit(:keyword, :source, :kind, :expdate, :info)
-    end
+
+    @categories_to_show = params[:categories].keys
+    @promotion = Promotion.with_category(@categories_to_show)
+    @promotion = if ''.eql?(params[:sort_by])
+                   @promotion.order(upvotes: :desc)
+                 else
+                   @promotion.order(expdate: :desc)
+                 end
+    @exp_header = 'exp'.eql?(params[:sort_by]) ? 'hilite bg-warning' : ''
+  end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    promotion_params['category'] = Promotion.categories[promotion_params['category']]
+    @promotion = Promotion.create!(promotion_params)
+    redirect_to promotions_path
+  end
+
+  def show
+    id = params[:id]
+    @promotion = Promotion.find(id)
+  end
+
+  def destroy
+    @promotion = Promotion.find(params[:id])
+    @promotion.destroy
+    redirect_to promotions_path
+  end
+
+  def edit
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def update
+    @promotion = Promotion.find params[:id]
+    promotion_params['category'] = Promotion.categories[promotion_params['category']]
+    @promotion.update_attributes!(promotion_params)
+    redirect_to promotion_path(@promotion)
+  end
+
+  private
+
+  def promotion_params
+    params.require(:promotion).permit(:keyword, :source, :category, :expdate, :info)
+  end
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,19 @@
 class Promotion < ApplicationRecord
+  enum category: { 'New User Only': 0, 'Refer Code': 1, Common: 2, Other: 3 }
+
+  def self.all_categories
+    ['New User Only', 'Refer Code', 'Common', 'Other']
+  end
+
+  def self.with_category(categories_array)
+    # if categories_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those categories
+    # if categories_list is nil, retrieve ALL movies
+    if categories_array.nil? || categories_array.length.zero?
+      Promotion.all
+    else
+      categories_array.map { |a| categories[a] }
+      Promotion.where(category: categories_array)
+    end
+  end
 end
